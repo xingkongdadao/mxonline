1300509ea698255e4a0d8f1e8926d22718aed694
lijiang <xingkongdadao@gmail.com>
2018-07-03 00:07:06 +0700

    后台标题，主题设置。

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -1,8 +1,20 @@
 
 
 import xadmin
+from xadmin import views
 
 from .models import *
+
+# 主题设置
+class BaseSetting(object):
+    enable_themes = True
+    use_bootswatch = True
+
+# 标题设置
+class GlobalSetting(object):
+    site_title = "大脸后台管理系统"
+    site_footer = "大脸教育网"
+    menu_style = "accordion"
 
 
 class EmailVerifyRecordAdmin(object):
@@ -20,3 +32,5 @@ class BannerAdmin(object):
 
 xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
 xadmin.site.register(Banner, BannerAdmin)
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+xadmin.site.register(views.CommAdminView, GlobalSetting)
