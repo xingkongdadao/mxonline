9c651176cd3821e83b1f6dfe4d94e57a486ff269
lijiang <xingkongdadao@gmail.com>
2018-07-02 14:39:35 +0700

    在usersAPP中增加， EmailVerifyRecord，banner两个模型。

--- users/models.py
+++ users/models.py
@@ -1,9 +1,13 @@
+from datetime import datetime
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
+
 # Create your models here.
 
 
-#继承修改用户表
+# 继承修改用户表
 
 
 class UserProfile(AbstractUser):
@@ -20,3 +24,32 @@ class UserProfile(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+# 邮箱验证模型
+class EmailVerifyRecord(models.Model):
+    code = models.CharField(max_length=20, verbose_name='验证码')
+    email = models.EmailField(max_length=50, verbose_name='邮箱')
+    send_type = models.CharField(verbose_name='验证码类型', choices=(('register', '注册'), ('forget', '找回密码'), (
+        'update_email', '修改邮箱')), max_length=30)
+    send_time = models.DateTimeField(verbose_name='发送时间', default=datetime.now)
+
+    class Meta:
+        verbose_name = '邮箱验证码'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return '{0}({1})'.format(self.code, self.email)
+
+
+# 轮播图
+class Banner(models.Model):
+    title = models.CharField(max_length=100, verbose_name='标题')
+    image = models.ImageField(upload_to='banner/%Y/%m', verbose_name='轮播图', max_length=100)
+    url = models.URLField(max_length=200, verbose_name='访问地址')
+    index = models.IntegerField(default=100, verbose_name="顺序")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+
+    class Meta:
+        verbose_name = "轮播图"
+        verbose_name_plural = verbose_name
