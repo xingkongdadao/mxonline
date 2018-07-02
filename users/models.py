53fb488313275e11a7e00caffff1a58784b41778
lijiang <xingkongdadao@gmail.com>
2018-07-02 14:12:00 +0700

    user模型性别增加字段长度。

--- users/models.py
+++ users/models.py
@@ -9,7 +9,7 @@ from django.contrib.auth.models import AbstractUser
 class UserProfile(AbstractUser):
     nick_name = models.CharField(max_length=50, verbose_name='昵称', default='')
     birthday = models.DateField(verbose_name='生日', blank=True, null=True)
-    gender = models.CharField(choices=(("male", "男"), ("female", "女")), default='female')
+    gender = models.CharField(choices=(("male", "男"), ("female", "女")), max_length=10, default='female')
     address = models.CharField(max_length=100, default='')
     mobile = models.CharField(max_length=20, null=True, blank=True)
     image = models.ImageField(upload_to='image/%Y/%m', default='image/default.png', max_length=100)
