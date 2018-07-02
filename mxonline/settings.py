bd8259947a51f8f4d6119146718c7658b39c75d0
lijiang <xingkongdadao@gmail.com>
2018-07-02 13:16:52 +0700

    更换数据库，mysql

--- mxonline/settings.py
+++ mxonline/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'mxonline.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mxonline',
+        'USER': 'lijiang',
+        'PASSWORD': 'LIjiang20060707',
+        'HOST': '3306',
     }
 }
 
