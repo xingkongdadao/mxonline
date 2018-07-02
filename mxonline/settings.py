ed2366f72b44e41630ca75272ea34c4a8b459b6a
lijiang <xingkongdadao@gmail.com>
2018-07-02 13:48:35 +0700

    重载 AUTH_USER_MODEL = 'users.UserProfile'

--- mxonline/settings.py
+++ mxonline/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'users',
 ]
 
+AUTH_USER_MODEL = 'users.UserProfile'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
